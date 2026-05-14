5d665389400bf23854300a7c2a9fb66f787c3981
Snehal <singhsnehal.2k1@gmail.com>
2026-05-14 15:08:30 +0530

    Added simple RBAC - using dependency injection to check if user perms is in a list of   allowed perms for accessing resource - 403 error if not allowed - updated PG table via alembic migration

--- 03-beyond-crud/src/auth/dependencies.py
+++ 03-beyond-crud/src/auth/dependencies.py
@@ -1,9 +1,16 @@
 from fastapi.security import HTTPBearer
-from fastapi import Request, status
+from fastapi import Request, status, Depends
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
 from src.db.redis import is_token_in_blacklist
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .service import UserService
+from .models import User
+
+
+user_service = UserService()
 
 
 class TokenBearer(HTTPBearer):
@@ -61,3 +68,25 @@ class RefreshTokenBearer(TokenBearer):
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail='Please provide an refresh token'
             )
+
+async def get_curr_user(
+    token_details: dict = Depends(AccessTokenBearer()),
+    session: AsyncSession = Depends(get_session)
+):
+    user_email = token_details['user']['user_email']
+    user = await user_service.get_user_by_email(user_email, session)
+    return user
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: list[str]) -> None:
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: User = Depends(get_current_user)):
+        if current_user.user_role in self.allowed_roles:
+            return True
+
+        return HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You are not permitted to access the resource'
+        )
--- 03-beyond-crud/src/books/routes.py
+++ 03-beyond-crud/src/books/routes.py
@@ -4,7 +4,7 @@ from .schemas import Book, BookCreate, BookUpdate
 from src.books.service import BookService
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
-from src.auth.dependencies import AccessTokenBearer
+from src.auth.dependencies import AccessTokenBearer, RoleChecker
 
 
 ##### Router Object
@@ -12,6 +12,7 @@ from src.auth.dependencies import AccessTokenBearer
 book_router = APIRouter(prefix='/books')
 book_service = BookService()
 access_token_bearer = AccessTokenBearer()
+role_checker = RoleChecker(['admin', 'user'])
 
 ##### Routes
 
@@ -25,7 +26,8 @@ async def get_headers(
 @book_router.get('/', response_model=list[Book])
 async def get_all_books(
     session: AsyncSession = Depends(get_session),
-    user_details = Depends(access_token_bearer)
+    user_details = Depends(access_token_bearer),
+    _:bool = Depends(role_checker)
 ):
     books = await book_service.get_all_books(session)
     return books
@@ -35,7 +37,8 @@ async def get_all_books(
 async def create_book(
     book_data: BookCreate,
     session: AsyncSession = Depends(get_session),
-    user_details = Depends(access_token_bearer)
+    user_details = Depends(access_token_bearer),
+    _:bool = Depends(role_checker)
 ) -> dict:
     new_book = await book_service.create_book(book_data, session)
     return new_book
@@ -45,7 +48,9 @@ async def create_book(
 async def get_book(
     book_uid: str,
     session: AsyncSession = Depends(get_session),
-    user_details = Depends(access_token_bearer)
+    user_details = Depends(access_token_bearer),
+    _:bool = Depends(role_checker)
+
 ) -> dict:
     book = await book_service.get_book(book_uid, session)
     if not book:
@@ -61,7 +66,9 @@ async def update_book(
     book_uid: str,
     book_update: BookUpdate,
     session: AsyncSession = Depends(get_session),
-    user_details = Depends(access_token_bearer)
+    user_details = Depends(access_token_bearer),
+    _:bool = Depends(role_checker)
+
 ) -> dict:
     updated_book = await book_service.update_book(book_uid, book_update, session)
     if not updated_book:
@@ -76,7 +83,9 @@ async def update_book(
 async def delete_book(
     book_uid: str,
     session: AsyncSession = Depends(get_session),
-    user_details = Depends(access_token_bearer)
+    user_details = Depends(access_token_bearer),
+    _:bool = Depends(role_checker)
+
 ):
     book_delete = await book_service.delete_book(book_uid, session)
     if book_delete is None:
